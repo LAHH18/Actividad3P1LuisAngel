646f55eb8e4993e577601c6e42d71ad88334731d
LA18 <luisangelhernandez02605@gmail.com>
2025-01-22 22:07:54 -0600

    A3P1

---
+++ JS/funciones.js
@@ -0,0 +1,24 @@
+"use strict";
+function calculate(operation) {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    let resultado = 0;
+    if (isNaN(num1) || isNaN(num2)) {
+        resultado = "Por favor, ingrese números válidos.";
+    }
+    else {
+        if (operation === "suma") {
+            resultado = num1 + num2;
+        }
+        else if (operation === "resta") {
+            resultado = num1 - num2;
+        }
+        else if (operation === "multiplicacion") {
+            resultado = num1 * num2;
+        }
+        else if (operation === "division") {
+            resultado = num2 !== 0 ? num1 / num2 : "Error matemático";
+        }
+    }
+    document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
+}
